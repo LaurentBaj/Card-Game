ce3b3c14baf4fbf01bbde819107250b7a4597420
LaurentBaj <laurent_baj@hotmail.com>
2020-11-09 20:41:18 +0100

    Visual change in terminal

--- Card-Game/Program.cs
+++ Card-Game/Program.cs
@@ -7,11 +7,15 @@ namespace Card_Game
     {
         static void Main(string[] args)
         {
-            Deck d1 = new Deck();
-            Console.WriteLine(d1.deck.Count);
+            Deck d1 = new Deck(); // Create deck
 
+            // Amount of cards before game starts
+            Console.WriteLine("(Card count: " + d1.deck.Count + ")\n"); 
+
+            // Choose amount of players
             int numberOfPlayers = 0;
-            Console.WriteLine("Choose the amount of players (2-4)");
+            Console.WriteLine("WELCOME TO THE CRAZY CARD GAME!");
+            Console.WriteLine("\r\nChoose the amount of players (2-4)\n");
             while (numberOfPlayers > 4 || numberOfPlayers < 2)
             {
                 numberOfPlayers = Convert.ToInt32(Console.ReadLine());
@@ -23,6 +27,9 @@ namespace Card_Game
 
             Console.WriteLine("");
 
+
+            // Create players based on user-input 
+            // Give them each a full hand
             PlayerFactory a = new PlayerFactory(numberOfPlayers);
             for (int i = 0; i < numberOfPlayers; i++)
             {
@@ -34,19 +41,23 @@ namespace Card_Game
 
             Console.WriteLine(""); 
 
+            // Print each player and their cards
+            // For testing
             for (int i = 0; i < numberOfPlayers; i++)
             {
-                Console.WriteLine("");
-                Console.WriteLine(PlayerFactory.players[i].Name); 
+                Console.WriteLine("\n" + PlayerFactory.players[i].Name);
+                Console.Write("[");
                 for (int j = 0; j < 4; j++)
                 {
                     Console.Write(PlayerFactory.players[i]._hand[j] + ", ");
                 }
+                Console.Write("\b\b]\n");
             }
 
             Console.WriteLine("\n");
-            d1.PrintDeck();
-            Console.WriteLine(d1.deck.Count);
+
+            //Deck count after first delegation
+            Console.WriteLine("\n(Card count after every player receives a hand: " +  d1.deck.Count + ")"); 
 
 
 
