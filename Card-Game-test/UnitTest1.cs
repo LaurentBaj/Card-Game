52f42d6d421ce58cbe416c0cd9a7b73e05e4438d
dannyblueman <dhb_90@hotmail.com>
2020-11-15 21:41:52 +0100

    Added NUnit test project

---
+++ Card-Game-test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Card_Game_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
