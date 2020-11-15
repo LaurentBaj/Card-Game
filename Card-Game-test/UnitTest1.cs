0b24633cb02c174c39be18987075fa6277650008
LaurentBaj <laurent_baj@hotmail.com>
2020-11-15 22:49:43 +0100

    Added NUnit

--- Card-Game-test/UnitTest1.cs
+++ Card-Game-test/UnitTest1.cs
@@ -1,18 +1,14 @@
+using Card_Game;
 using NUnit.Framework;
 
 namespace Card_Game_test
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            
         }
     }
 }
